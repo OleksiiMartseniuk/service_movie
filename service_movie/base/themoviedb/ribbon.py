2123654896ae3c91afa9628c2b8a1eed11d97d66
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-10-12 10:37:24 +0300

    add method get_count_page

--- service_movie/base/themoviedb/ribbon.py
+++ service_movie/base/themoviedb/ribbon.py
@@ -36,3 +36,33 @@ class MovieApi:
             if item:
                 result.append(item)
         return result
+
+    async def _get_count_page(
+        self, action: int, region: str = 'RU'
+    ) -> int | None:
+        """Получения количества страниц"""
+        match action:
+            case 1:
+                data = await self.client.get_top_rating_movie(region=region)
+            case 2:
+                data = await self.client.get_popular_movie(region=region)
+            case 3:
+                data = await self.client.get_upcoming_movie(region=region)
+            case 4:
+                data = await self.client.get_top_rating_tv()
+            case 5:
+                data = await self.client.get_popular_tv()
+            case _:
+                logger.error(f'Неверный action[{action}]')
+                return None
+
+        if not data:
+            logger.error(f'Нет данных action[{action}]')
+            return None
+
+        try:
+            count = data['total_pages']
+        except KeyError:
+            logger.error('Нет ключа [total_pages]')
+            return None
+        return count

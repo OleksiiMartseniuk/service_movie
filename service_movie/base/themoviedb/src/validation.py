2094f2aec7f98db915ff3ef1669aca56e98e8644
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-10-12 06:53:25 +0300

    path directory add src

---
+++ service_movie/base/themoviedb/src/validation.py
@@ -0,0 +1,22 @@
+import logging
+
+from typing import TypeVar, Type
+
+from pydantic import BaseModel, ValidationError
+
+
+logger = logging.getLogger(__name__)
+ModelSchemaType = TypeVar('ModelSchemaType', bound=BaseModel)
+
+
+def get_schemas(
+    data: dict,
+    schema_model: Type[ModelSchemaType]
+) -> ModelSchemaType | None:
+    """Получения схемы"""
+    try:
+        result = schema_model(**data)
+    except ValidationError as exc:
+        logger.error(f'{exc.__class__} {str(exc)}')
+        return None
+    return result

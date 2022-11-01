d67e9c8d49d9c7788761a97f39db7b3e3226d74f
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-11-01 20:59:33 +0200

    add data params

--- tests/test_themoviedb/config_data.py
+++ tests/test_themoviedb/config_data.py
@@ -1,5 +1,12 @@
 from service_movie.base.themoviedb import schemas
 
+params_default = {'api_key': 'test_token', 'language': 'ru-Ru'}
+params_data = {'params1': 'params1', 'params2': None}
+params_result = {
+    'api_key': 'test_token',
+    'language': 'ru-Ru',
+    'params1': 'params1'
+}
 
 genres = {
     'genres':

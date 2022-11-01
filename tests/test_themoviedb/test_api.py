b55e2180ccfcded5de9994dce1c8919307ebf367
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-11-01 19:33:24 +0200

    add test method get and get_genres

---
+++ tests/test_themoviedb/test_api.py
@@ -0,0 +1,36 @@
+import pytest
+import httpx
+
+from . import config_data
+
+
+@pytest.mark.asyncio
+async def test_get(mocker, client_api):
+    mocker.patch(
+        'service_movie.base.themoviedb.api.httpx.AsyncClient.get',
+        return_value=httpx.Response(200, json={'test': 'test'})
+    )
+    result = await client_api.get('https://test-api')
+    assert result == {'test': 'test'}
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('status_code', [300, 400, 500])
+async def test_get_status_code(status_code, mocker, client_api):
+    mocker.patch(
+        'service_movie.base.themoviedb.api.httpx.AsyncClient.get',
+        return_value=httpx.Response(status_code, json={'error': 'error'})
+    )
+    result = await client_api.get('https://test-api')
+    assert result is None
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('tv', [True, False])
+async def test_get_genres(tv, mocker, client_api):
+    mocker.patch(
+        'service_movie.base.themoviedb.api.TheMovieDatabaseApi.get',
+        return_value=config_data.genres
+    )
+    result = await client_api.get_genres(tv=tv)
+    assert config_data.genres == result

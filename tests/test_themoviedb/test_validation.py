dbfabf737c9c6dc60fda0e2a0ad740710052ab7d
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-11-02 15:40:53 +0200

    edit test validation add parametrize

--- tests/test_themoviedb/test_validation.py
+++ tests/test_themoviedb/test_validation.py
@@ -1,19 +1,37 @@
+import pytest
+
 from service_movie.base.themoviedb import validation, schemas
 
 from . import config_data
 
 
-def test_get_schemas():
-    data = validation.get_schemas(
-        data=config_data.genres,
-        schema_model=schemas.Genres
+@pytest.mark.parametrize(
+    'data, schema_model, result',
+    [
+        (config_data.genres, schemas.Genres, config_data.genres_schema),
+        (config_data.movie, schemas.Movie, config_data.movie_schema),
+        (config_data.tv, schemas.TV, config_data.tv_schema),
+    ]
+)
+def test_get_schemas(data, schema_model, result):
+    result_data = validation.get_schemas(
+        data=data,
+        schema_model=schema_model
     )
-    assert data == config_data.genres_schema
+    assert result_data == result
 
 
-def test_get_schemas_not_validation():
-    data = validation.get_schemas(
-        data={'genre_1': 'one', 'genre_2': 'two'},
-        schema_model=schemas.Genres
+@pytest.mark.parametrize(
+    'data, schema_model',
+    [
+        (config_data.genres, schemas.TV),
+        (config_data.movie, schemas.MovieResult),
+        (config_data.tv, schemas.TopRatedTV),
+    ]
+)
+def test_get_schemas_not_validation(data, schema_model):
+    result = validation.get_schemas(
+        data=data,
+        schema_model=schema_model
     )
-    assert data is None
+    assert result is None

8b8db74a6245335eefe20eeb86aaf723aead249e
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-11-05 16:30:18 +0200

    create table tv adn m2m

--- service_movie/app/models/movie.py
+++ service_movie/app/models/movie.py
@@ -155,3 +155,109 @@ spoken_languages = sqlalchemy.Table(
     sqlalchemy.Column("iso_639_1", sqlalchemy.String(20)),
     sqlalchemy.Column("name", sqlalchemy.String(30)),
 )
+
+
+# m2m таблица сериал и жанров
+tv_genre = sqlalchemy.Table(
+    "tv_genre",
+    metadata,
+    sqlalchemy.Column("tv_id", sqlalchemy.ForeignKey("tv.id")),
+    sqlalchemy.Column("genre_id", sqlalchemy.ForeignKey("genres.id")),
+)
+
+
+# m2m таблица сериал и сети
+tv_networks = sqlalchemy.Table(
+    "tv_networks",
+    metadata,
+    sqlalchemy.Column("tv_id", sqlalchemy.ForeignKey("tv.id")),
+    sqlalchemy.Column("network_id", sqlalchemy.ForeignKey("networks.id")),
+)
+
+
+# m2m таблица сериал и страны производства
+tv_production_countries = sqlalchemy.Table(
+    "tv_production_countries",
+    metadata,
+    sqlalchemy.Column("tv_id", sqlalchemy.ForeignKey("tv.id")),
+    sqlalchemy.Column(
+        "production_countries_id",
+        sqlalchemy.ForeignKey("production_countries.id")
+    ),
+)
+
+
+# m2m таблица сериал и производственные компании
+tv_production_companies = sqlalchemy.Table(
+    "tv_production_companies",
+    metadata,
+    sqlalchemy.Column("tv_id", sqlalchemy.ForeignKey("tv.id")),
+    sqlalchemy.Column(
+        "production_companies_id",
+        sqlalchemy.ForeignKey("production_companies.id")
+    ),
+)
+
+
+# m2m таблица сериал и сезон
+tv_seasons = sqlalchemy.Table(
+    "tv_seasons",
+    metadata,
+    sqlalchemy.Column("tv_id", sqlalchemy.ForeignKey("tv.id")),
+    sqlalchemy.Column("seasons_id", sqlalchemy.ForeignKey("seasons.id")),
+)
+
+
+# m2m таблица сериал и разговорные языки
+tv_spoken_languages = sqlalchemy.Table(
+    "tv_spoken_languages",
+    metadata,
+    sqlalchemy.Column("tv_id", sqlalchemy.ForeignKey("tv.id")),
+    sqlalchemy.Column(
+        "spoken_languages_id",
+        sqlalchemy.ForeignKey("spoken_languages.id")
+    ),
+)
+
+
+# Таблица сериал
+tv = sqlalchemy.Table(
+    "tv",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer(), primary_key=True),
+    sqlalchemy.Column("id_tv", sqlalchemy.Integer()),
+    sqlalchemy.Column("backdrop_path", sqlalchemy.String(255)),
+    # list[int] episode_run_time
+    sqlalchemy.Column("episode_run_time", sqlalchemy.Integer()),
+    sqlalchemy.Column("first_air_date", sqlalchemy.Date()),
+    sqlalchemy.Column("homepage", sqlalchemy.String(255)),
+    sqlalchemy.Column("in_production", sqlalchemy.Boolean()),
+    # list[str] languages
+    sqlalchemy.Column("languages", sqlalchemy.String(255)),
+    sqlalchemy.Column("last_air_date", sqlalchemy.Date()),
+    sqlalchemy.Column(
+        "last_episode_to_air",
+        sqlalchemy.ForeignKey('last_episode_to_air.id')
+    ),
+    sqlalchemy.Column("name", sqlalchemy.String(255)),
+    sqlalchemy.Column("number_of_episodes", sqlalchemy.Integer()),
+    sqlalchemy.Column("number_of_seasons", sqlalchemy.Integer()),
+    # list[str] origin_country
+    sqlalchemy.Column("origin_country", sqlalchemy.String(255)),
+    sqlalchemy.Column("original_language", sqlalchemy.String(20)),
+    sqlalchemy.Column("original_name", sqlalchemy.String(255)),
+    sqlalchemy.Column("overview", sqlalchemy.Text()),
+    sqlalchemy.Column("popularity", sqlalchemy.Float()),
+    sqlalchemy.Column("poster_path", sqlalchemy.String(255)),
+    sqlalchemy.Column("status", sqlalchemy.String(100)),
+    sqlalchemy.Column("tagline", sqlalchemy.String(255)),
+    sqlalchemy.Column("type", sqlalchemy.String(100)),
+    sqlalchemy.Column("vote_average", sqlalchemy.Float()),
+    sqlalchemy.Column("vote_count", sqlalchemy.Integer()),
+    # genres m2m
+    # networks m2m
+    # production_companies m2m
+    # production_countries m2m
+    # seasons m2m
+    # spoken_languages m2m
+)
